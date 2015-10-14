d2010e3a17e32de3a4f13182b918872e891736eb
Micahel Turner <aztex87068@juno.com>
2015-10-14 17:00:58 -0600

    added idex.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html lang="en">
+	<head>
+		<meta charset="utf"/>.
+		<title>mr-porter product page</title>
+	</head>
+	<body>
+		<h1>persona</h1>
+		<p>
+			affluent male ,27-56 years old,urban dweller,status & quality driven,he know his sizing,
+			knows brands & wants products shiipped to his home or friends.
+		</p>
+		<h1>use case 1</h1>
+		<ul>
+			<li>goal #1 looking for a blazer.</li>
+			<li>step 1 goes to mr-porter.com</li>
+			<li>step 2 looks at clothing from top nav bar</li>
+			<li>step 3selects blazersfrom the dropdown menu</li>
+			<li>step 4 selects a specific blazer item</li>
+			<li>step 5 clicks on product for more detailed info</li>
+			<l> allows me to buy from given info.</l>
+		</ul>
+		<h1>use case 2</h1>
+		<ul>
+			<li>goal #2 looking for a gift.</li>
+			<li>step 1 select a gift from nav bar</li>
+			<li>step 2 select " books" catagory</li>
+			<li>step 3 select by publisher Taschen</li>
+			<li>step 4 takes me to product specific info</li>
+			<li>step 5 allows me to purchase.</li>
+			<li></li>
+		</ul>
+	</body>
+</html>
