f737365eacf68fd7d1237e8d09b5901d3ad5f8c9
Micahel Turner <aztex87068@juno.com>
2015-10-15 13:10:31 -0600

    changed an entity

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<meta charset="utf-8"/>.
-
+		<meta charset="utf-8"/>
 		<title>mr-porter product page</title>
 	</head>
 	<body>
 		<h1>persona</h1>
 		<p>
-			Dylan is an affluent male, 27-56years old,urban dweller,status & quality driven,he know his sizing,
-			knows brands & wants products shipped  to his home or friends.Usually a business owner/executive,very tech savy
-			likes a faster -sleeker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro )
-		</p>
+			Dylan is an affluent male, 27-56 years old, urban dweller, status & quality driven, he know his sizing, knows brands & wants products shipped  to his home or friends.Usually a business owner/executive,very tech savy likes a faster -sleeker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro ).</p>
 		<h1>use case 1</h1>
 		<ul>
 			<li>goal #1 looking for a blazer.</li>
@@ -27,20 +24,36 @@
 			<li>goal #2 looking for a gift.</li>
 			<li>step 1 select a gift from nav bar</li>
 			<li>nav bar gives me a drop down menu</li>
-			<li>step 2 select " books" catagory from the nav bar</li>
+			<li>step 2 select "books" catagory from the nav bar</li>
 			<li>step 3 select by publisher Taschen from the drop down</li>
 			<li>step 4 takes me to product specific info</li>
 			<li>step 5 there is a purchase option area on the page.</li>
-			<li></li>
 		</ul>
 		<h1>Conceptual Model</h1>
 			<ul>
-				<li>Brands & Products
-				<li>Brands can have names & descriptions</li>
-				<li>products can have prices, descriptions, names, brands</li>
-				<li>each Brand can have many products</li>
-				<li>each product can have only one brand</li>
+
+
+
 			</ul>
+		<strong>Entity: Brand</strong>
+		<ul>
+			<li>Brand ID</li>
+			<li>Brand Name</li>
+			<li>Description</li>
+			<li>Products</li>
+		</ul>
+		<p><strong>Relationship:</strong> Each Brand can have many Products. (One to many relationship.)</p>
+		<strong>Entity:Product</strong>
+		<ul>
+			<li>Brand</li>
+			<li>Product  ID</li>
+			<li>Description</li>
+			<li>Price</li>
+			<li>catagorie</li>
+			<name></name>
+		</ul>
+		<p>Each product can have only one brand. (one to one relationship.)</p>
+
 
 	</body>
 </html>
