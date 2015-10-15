1b2f2d3595f924058ef102283969efefcb6a370d
Micahel Turner <aztex87068@juno.com>
2015-10-15 09:08:19 -0600

    made spelling changes

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html lang="en">
 	<head>
-		<meta charset="utf"/>.
+		<meta charset="utf-8"/>.
+
 		<title>mr-porter product page</title>
 	</head>
 	<body>
 		<h1>persona</h1>
 		<p>
 			affluent male ,27-56 years old,urban dweller,status & quality driven,he know his sizing,
-			knows brands & wants products shiipped to his home or friends.Usually a business owner/executive,very tech savy
-			likes a faster -slleker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro )
+			knows brands & wants products shipped  to his home or friends.Usually a business owner/executive,very tech savy
+			likes a faster -sleeker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro )
 		</p>
 		<h1>use case 1</h1>
 		<ul>
 			<li>goal #1 looking for a blazer.</li>
 			<li>step 1 goes to mr-porter.com</li>
 			<li>step 2 looks at clothing from top nav bar</li>
-			<li>step 3selects blazersfrom the dropdown menu</li>
+			<li>step 3selects blazers from the drop down menu</li>
 			<li>step 4 selects a specific blazer item</li>
 			<li>step 5 clicks on product for more detailed info</li>
 			<l> allows me to buy from given info.</l>
