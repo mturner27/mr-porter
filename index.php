3abbf1dbeb59d866e24e635d813f8636db7e9b13
Micahel Turner <aztex87068@juno.com>
2015-10-15 10:17:01 -0600

    re-wrote some language

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>persona</h1>
 		<p>
-			affluent male , 27-56years old,urban dweller,status & quality driven,he know his sizing,
+			Dylan is an affluent male, 27-56years old,urban dweller,status & quality driven,he know his sizing,
 			knows brands & wants products shipped  to his home or friends.Usually a business owner/executive,very tech savy
 			likes a faster -sleeker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro )
 		</p>
@@ -17,9 +17,10 @@
 			<li>step 1 goes to mr-porter.com</li>
 			<li>step 2 looks at clothing from top nav bar</li>
 			<li>step 3selects blazers from the drop down menu</li>
+			<li>the displayes a list of blazers</li>
 			<li>step 4 selects a specific blazer item</li>
-			<li>step 5 clicks on product for more detailed info</li>
-			<l> allows me to buy from given info.</l>
+			<li>step 5 this step allows for more detailed info</li>
+			<li> allows me to buy from given info.</li>
 		</ul>
 		<h1>use case 2</h1>
 		<ul>
